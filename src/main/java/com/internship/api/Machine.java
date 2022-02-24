6d9e6217b25ca714b9762b8553cf4d1003890f93
Yalov Vasiliy <yalov.vasiliy@gmail.com>
2022-02-24 10:57:58 +0300

    Remove log4j

--- src/main/java/com/internship/api/Machine.java
+++ src/main/java/com/internship/api/Machine.java
@@ -1,13 +1,8 @@
 package com.internship.api;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.util.*;
 
 public class Machine {
-    private static final Logger log = LoggerFactory.getLogger(Machine.class);
-
     private Integer coffeeContainer;
     private Integer waterContainer;
     private Integer milkContainer;
